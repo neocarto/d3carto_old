b08c14842325d546bf3214d4c4f6296d61c60b64
neocarto <nicolas.lambert@ums-riate.fr>
2016-02-04 14:45:23 +0100

    A static map of Europe

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # d3carto
-Here, i try to explain how to make intercative and animatied thematic maps with d3.js.
+Here, i try to explain how to make intercative and animated thematic maps with d3.js.
 It's a step by step work...
 
--- index.php
+++ index.php
@@ -1,3 +1,41 @@
+<?php
+// GALLERY
+$thumb[0] ="map_layers" ; $descr[0]="A 'static' map of Europe";
+?>
+
+
+<html>
+<head>
+<link href="css/style.css" rel="stylesheet" type="text/css" >  
+</head>
+<body>
+    <div class="note" id ="note"><b>Here find a gallery of maps designed with D3.js.</b> All sources are available on <a href ="https://github.com/neocarto/d3carto" target="_blank">Github</a>. <span class="small">CC (BY) N. Lambert (<a href ="http://neocarto.hypotheses.org/" target ="_blank">neocarto</a>).</span> </div>
+
+<?php
+
+for ($i=0; $i<count($thumb);$i++){
+	$item = '<div class="img">';	
+	$item .= "<a href='gallery/$thumb[$i].html'>";
+	$item .= "<img src='gallery/$thumb[$i].png' alt='$thumb[$i]' width='300' height='200'></a>";
+	$item .= "<div class='desc'>$descr[$i]</div></div>";
+	echo $item;
+
+	}
+?>
+
+
+
+  
+   
+  
+  
+
+
+
+
+
+
+<!--
 <table width='190px' height='190px'>
   <tr>
 
@@ -19,3 +57,8 @@ while($file = readdir($dir)) {
 closedir($dir); 
 ?>
 </table>
+-->
+
+
+</body>
+</html>
